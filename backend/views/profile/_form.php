f1fdbe1ee1e62162c86975529d1b6fba858ce541
GuzmanT <guzmantut@gmail.com>
2016-10-14 18:07:30 -0500

    from profile

--- backend/views/profile/_form.php
+++ backend/views/profile/_form.php
@@ -13,7 +13,7 @@ use yii\jui\DatePicker;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <!--?= $form->field($model, 'user_id')->textInput(['maxlength' => true]) ?-->
+    <?= $form->field($model, 'user_id')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'first_name')->textarea(['rows' => 6]) ?>
 
